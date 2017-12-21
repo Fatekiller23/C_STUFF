a41484a022cc460871ffe84303f5b505b233efc2
Your Name <mgsf@163.com>
2017-12-21 13:17:16 +0800

    更新 excercise_1-23.c 自己的解答

--- the_C_programming_language_solution/Chapter_1/excercise_1-23.c
+++ the_C_programming_language_solution/Chapter_1/excercise_1-23.c
@@ -3,61 +3,164 @@ Exercise 1-23. Write a program to remove all comments from a C program. Don't fo
 handle quoted strings and character constants properly. C comments don't nest.
 */
 
-/*
-怎样从文件中导入字符串？
-cmd 中 用 < 重定向符
-*/
 
 #include <stdio.h>
 #define MAXLEN 100000
 #define BACKSLASH 47
+#define YES 1
+#define NO 0
 
-int main(){
-    int first, second, now;
-    first = ' ';
-    second = ' ';
-    int inline_comment = 0;
-    int multipli_comment = 0;
-    int count =0 ;
-    while ((now = getchar()) != EOF ) {
-        // count += 1;
-        // if (count < 2){
-        //     continue;
-        // }
-        // 如果两个falg都是0，则打印该字符，若不是0 则跳过该字符。
-        // printf("last : %c | c : %c | inline: %d | multiple: %d \n", last,c,inline_comment,multipli_comment);
-        if (multipli_comment == 0 && inline_comment == 0) {
-            // 判断 inline comment 的条件
-            if (first == BACKSLASH && second == BACKSLASH ){
-                inline_comment = 1;
-            }
-            // 判断  multipli comment  的 条件
-            if (first == BACKSLASH && second == '*'){
-                multipli_comment = 1;
-            }
-        }
-        else if (multipli_comment == 1){
-            if (first =='*' && second == BACKSLASH){
-                multipli_comment = 0;
-            }
-        }
-        else if (inline_comment == 1){
+
+int getline(char s[],int lim);
+void copy(char to[], char from[]);
+void put_raw_char(char c);
+void print_raw_arry(char s[]);
+void strip_newline(char s[]);
+
+
+/* print the longest input line */
+main()
+{
+    int inline_comment, mul_comment = NO;
+    int len; /* current line length */
+    char line[MAXLEN]; /* current input line */
+    char result[MAXLEN]; /* current input line */
+    int j=0;
+    while ((len = getline(line, MAXLEN)) > 0) {
+        inline_comment = NO;
+        int first=line[0];
+        int i;
+        for (i=1; i<len; i++) {
             
-            if (second == '\n'){
-                // printf("2\n");
-                inline_comment =0;
-                // last = getchar();
+            // printf("first is %c, now is %c \n", first, line[i]);
+            if (line[i] == BACKSLASH && first == BACKSLASH) {
+                inline_comment = YES;
+            }
+            
+            if (first == BACKSLASH && line[i] == '*') {
+                mul_comment = YES;
+            }
+
+            if (first == '*' && line[i] == BACKSLASH && mul_comment == YES) {
+                mul_comment = NO;
+                i=i+1;
+                first = line[i];
+
+            }
+            // printf("inline_comment: %d , mul_comment: %d\n\n", inline_comment, mul_comment);
+            if (inline_comment == YES)
+                break;
+
+            if (mul_comment == YES){
+                first = line[i];
+                continue;
+            
+            }
+
+            
+            // put_raw_char(first);
+            result[j] = first;
+            j++;   
+            first = line[i];
+        }
+        result[j] = '\n';
+        j++;
+    }
+    result[j+1] = '\0';
+    // print_raw_arry(result);
+    strip_newline(result);
+    return 0;
+}
+/* getline: read a line into s, return length */
+int getline(char s[],int lim) {
+    int c, i;
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
+
+void put_raw_char(char c) {
+        if (c =='\t'){
+            printf("\\t");
+        }
+        else if(c == '\b'){
+            printf("\\b");
+        }
+        else if(c =='\\'){
+            printf("\\");
+        }
+        else if(c ==' '){
+            printf("\\s");
+        }
+        else if(c =='\n'){
+            printf("\\n");
+        }
+        else{
+            printf("%c",c);
+        }
+}
+
+void print_raw_arry(char s[]) {
+    int length=0;
+    int c;
+    while (s[length] != '\0')
+        ++length;
+    for (int i=0;i<length;i++){
+        c =s[i];
+        if (c =='\t'){
+            printf("\\t");
+        }
+        else if(c == '\b'){
+            printf("\\b");
+        }
+        else if(c =='\\'){
+            printf("\\");
+        }
+        else if(c ==' '){
+            printf("\\s");
+        }
+        else if(c =='\n'){
+            printf("\\n");
+        }
+        else{
+            printf("%c",c);
+        }
+    }
+}
+
+void strip_newline(char s[]) {
+    int i = 0;
+    while (s[i] == '\n')
+        i++;
+
+    int is_newline = 0;
+    
+    while (s[i] != '\0' ) {
+        if (s[i] =='\n'){
+
+            if (is_newline!=1){
+                printf("%c", s[i]);
+                is_newline =1;
             }
         }
-
-
-        // 如果两个falg都是0，则打印该字符，若不是0 则跳过该字符。
-        if (multipli_comment == 0 && inline_comment == 0) {
-            printf("%c", now);
-            // printf("output!");
+        else {
+            printf("%c", s[i]); 
+            is_newline =0;
         }
-
-        first = second;
-        second = now;
+        ++i;
     }
+   
 }
